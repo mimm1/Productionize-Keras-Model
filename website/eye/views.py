a6a9f6bb3c66be7cefaa2a9a8296e41d2af6311b
Muhammad Idrees <mobin_ksa@yahoo.com>
2021-01-18 22:23:02 +0500

    Add files via upload

---
+++ website/eye/views.py
@@ -0,0 +1,41 @@
+from django.http import HttpResponse
+from .models import Picture
+from django.template import loader
+from pathlib import Path
+from django.shortcuts import render
+from django.conf import settings
+from django.core.files.storage import FileSystemStorage
+
+
+def index(request):
+	return render(request, 'eye/index.html', {})
+
+def result(request):
+    if request.method == 'POST' and request.FILES['eyeimage']:
+        
+        import os
+        file = 'uploaded.jpg'
+        location = "C:/Users/OrhanEyhab/diabetic-retinopathy-master/website/eye/test_images/uploaded"
+        path = os.path.join(location, file)
+
+        PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
+        #CAPTHA_ROOT = os.path.join(PROJECT_PATH,'test_images','uploaded','uploaded.jpg')
+        os.remove(path)
+        #os.remove(CAPTHA_ROOT)
+
+        myfile = request.FILES['eyeimage']
+        fs = FileSystemStorage()
+        filename = fs.save('uploaded.jpg', myfile)
+        uploaded_file_url = fs.url(filename)
+        
+        uploaded_file_url = os.path.join(PROJECT_PATH,'test_images', 'uploaded', 'uploaded.jpg')
+        
+        from .classifier import prediction
+        tested, percent = prediction()
+        
+        return render(request, 'eye/results.html', {
+            'tested': tested, 'percent' : percent, 'uploaded_file_url' : uploaded_file_url
+        })
+    
+    
+    return render(request, 'eye/results.html')
